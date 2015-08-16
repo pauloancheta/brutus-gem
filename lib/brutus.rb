88bdc9a3fae907517817d854ae2caffd6de53e55
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-08-15 19:33:02 -0700

    added differentiated numbers to letters in decryption

--- lib/brutus.rb
+++ lib/brutus.rb
@@ -2,8 +2,12 @@ require 'digest'
 
 class Brutus
 
-  def self.decipher(key, digest)
+  def self.decipher_letters(key, digest)
     find_letter_match(key, digest)
+  end
+
+  def self.decipher_numbers(key, digest)
+    find_number_match(key, digest)
   end
 
   def self.cipher(key, message)
@@ -14,8 +18,25 @@ class Brutus
 
   def self.digest_match?(digest, comparer_digest)
     digest == comparer_digest 
-  end
+  end 
 
+  def self.find_number_match(key, digest)
+    current_number = 0
+    new_digest = cipher(key, current_number.to_s)
+    current_thousands = 1000
+    puts "currently searching through #{current_number % 1000} to #{current_thousands}"
+
+    while !digest_match?(digest, new_digest)
+      current_number += 1
+      new_digest = cipher(key, current_number.to_s)
+
+      if current_number == current_thousands
+        puts "currently searching through #{current_number % 1000} to #{current_thousands}"
+        current_thousands += 1000
+      end
+    end
+    puts "THE PASSWORD IS #{current_number}"
+  end
 
   def self.find_letter_match(key, digest)
     current_letter = "a"
@@ -32,7 +53,7 @@ class Brutus
       end
     end
 
-    current_letter
+    puts "THE PASSWORD IS: #{current_letter}"
   end
 
   def self.find_key(key)
